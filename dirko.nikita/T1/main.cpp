7df43e8f455032a0f727a0baddeafcc73c843662
Alastor20 <nik_d.007@mail.ru>
2026-03-27 16:21:25 +0300

    add add line

--- dirko.nikita/T1/main.cpp
+++ dirko.nikita/T1/main.cpp
@@ -1,37 +1,59 @@
+#include <iomanip>
 #include <iostream>
+#include <istream>
 #include <limits>
 #include <memory>
 #include <stdexcept>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 class Note
 {
 public:
   Note(std::string name):
     name(name),
-    desc(""),
+    desc(),
     links()
   {}
 
-  std::string name, desc;
+  std::string name;
+  std::vector< std::string > desc;
   std::unordered_map< std::string, std::weak_ptr< Note > > links;
 };
 
-void addNote(std::istream &is, std::unordered_map< std::string, std::shared_ptr< Note > > &db)
+using notes_t = std::unordered_map< std::string, std::shared_ptr< Note > >;
+
+void addNote(std::istream &is, notes_t &db)
 {
   std::string name;
   is >> name;
   db[name] = std::make_shared< Note >(name);
 }
 
+void addDesc(std::istream &is, notes_t &db)
+{
+  std::string name, desc;
+  is >> name;
+  is >> std::quoted(desc);
+  if (is.bad()) {
+    throw std::logic_error("not enought args");
+  }
+  try {
+    db.at(name)->desc.push_back(desc);
+  } catch (const std::out_of_range &) {
+    throw std::logic_error("no note with this name");
+  }
+}
+
 int main()
 {
-  std::unordered_map< std::string, std::shared_ptr< Note > > db;
+  notes_t db;
   constexpr std::streamsize streamMax = std::numeric_limits< std::streamsize >::max();
   using func_t = void (*)(std::istream &, std::unordered_map< std::string, std::shared_ptr< Note > > &);
   const std::unordered_map< std::string, func_t > cmds{
       {"note", addNote},
+      {"line", addDesc},
   };
   std::string cmd;
   while (std::cin >> cmd) {
