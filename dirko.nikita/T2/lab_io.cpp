ee0f7d6d37999d5102da28a545dabf957a76a577
Alastor20 <nik_d.007@mail.ru>
2026-05-24 00:18:38 +0300

    implement operator<

--- dirko.nikita/T2/lab_io.cpp
+++ dirko.nikita/T2/lab_io.cpp
@@ -45,6 +45,16 @@ std::istream &dirko::operator>>(std::istream &in, LabelIO &&dest)
   return in;
 }
 
+std::istream &dirko::operator>>(std::istream &in, CharIO &&dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+
+  return in;
+}
+
 std::istream &dirko::operator>>(std::istream &in, StringIO &&dest)
 {
   std::istream::sentry sentry(in);
@@ -52,4 +62,18 @@ std::istream &dirko::operator>>(std::istream &in, StringIO &&dest)
     return in;
   }
   return in >> std::quoted(dest.ref);
+}
+
+bool dirko::operator<(const DataStruct &lhs, const DataStruct &rhs)
+{
+  if (lhs.key1 != rhs.key1) {
+    return lhs.key1 < rhs.key1;
+  }
+  if (lhs.key2.real() != rhs.key2.real()) {
+    return lhs.key2.real() < rhs.key2.real();
+  }
+  if (lhs.key2.imag() != rhs.key2.imag()) {
+    return lhs.key2.imag() < rhs.key2.imag();
+  }
+  return lhs.key3.length() < rhs.key3.length();
 }
--- dirko.nikita/T2/lab_io.hpp
+++ dirko.nikita/T2/lab_io.hpp
@@ -52,5 +52,7 @@ namespace dirko
 
   std::istream &operator>>(std::istream &in, DataStruct &&dest);
   std::ostream &operator<<(std::ostream &out, const DataStruct &src);
+
+  bool operator<(const DataStruct &lhs, const DataStruct &rhs);
 }
 #endif
