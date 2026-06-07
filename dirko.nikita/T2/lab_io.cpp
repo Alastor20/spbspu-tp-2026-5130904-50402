40944ccf31243ec4c51c36ec411ff5521d08300a
Alastor20 <nik_d.007@mail.ru>
2026-06-07 17:59:20 +0300

    move ioguard and delimiter to common

--- dirko.nikita/T2/lab_io.cpp
+++ dirko.nikita/T2/lab_io.cpp
@@ -1,35 +1,7 @@
 #include "lab_io.hpp"
+#include <delim.hpp>
+#include <ioguard.hpp>
 #include <iomanip>
-
-std::istream &dirko::operator>>(std::istream &in, DelimIO &&dest)
-{
-  std::istream::sentry sentry(in);
-  if (!sentry) {
-    return in;
-  }
-  char c = '\0';
-  in >> c;
-  if (in && c != dest.exp) {
-    in.setstate(std::ios::failbit);
-  }
-  return in;
-}
-
-dirko::IOguard::IOguard(std::basic_ios< char > &s):
-  s_(s),
-  width_(s.width()),
-  precision_(s.precision()),
-  fmt_(s.flags()),
-  fill_(s.fill())
-{}
-
-dirko::IOguard::~IOguard()
-{
-  s_.width(width_);
-  s_.precision(precision_);
-  s_.flags(fmt_);
-  s_.fill(fill_);
-}
 
 std::istream &dirko::operator>>(std::istream &in, LabelIO &&dest)
 {
